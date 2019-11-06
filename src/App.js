720aef37af804aae828550bb1d63cff933e62b04
taslimy <taslimer@gmail.com>
2019-11-05 20:15:24 -0500

    Found an api pulling ins ome data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `movies?key=b0tLcG5KaWdvcW1jbUhPZ29xbWNtS0NpcVp5WVlhR1lwMkdhbXErR202S3FVMytjcHFlY29abz0`
+      )
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          movies: res.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
+    const x = this.state.movies;
+
     return (
       <div className='App'>
-        <span>Initial Commit</span>
+        <div>hello you working?</div>
+        {x.map((movies, index) => (
+          <div key={index}>
+            <a
+              target='_blank'
+              without
+              rel='noopener noreferrer'
+              href={`https://gomostream.com/movie/${movies.slug}`}
+            >
+              {movies.title}
+            </a>
+          </div>
+        ))}
       </div>
     );
   }
