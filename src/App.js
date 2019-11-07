b8cad0e0d970bc8e54b3321e4f7f566d364d6823
taslimy <taslimer@gmail.com>
2019-11-06 20:35:34 -0500

    whoops

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movies: [],
-      torrents: []
+      movies: []
     };
   }
 
   componentDidMount() {
     axios
-      .get(`movies/1`)
+      .get(
+        `movie/popular?api_key=30b8cc2868c889f56c4f30abefa4c5fd&language=en-US&page=1`
+      )
       .then(res => {
         console.log(res.data);
         this.setState({
-          movies: res.data,
-          torrents: res.data.torrents
+          movies: res.data.results
         });
       })
       .catch(err => {
@@ -29,24 +29,13 @@ class App extends Component {
 
   render() {
     const x = this.state.movies;
-    const torrents = this.state.torrents;
-    const ticket =
-      'https://videospider.in/getticket.php?key=aS8lAlSImxCzltfq&secret_key=uuhbhey4lq3dhq9qati52mhi2uul3g';
+
     return (
       <div className='App'>
         <div>hello you working?</div>
         {x.map((movies, index) => (
           <div key={index}>
             <h2>{movies.title}</h2>
-            <a
-              href={`https://videospider.stream/getvideo?key=aS8lAlSImxCzltfq&video_id=${movies._id}&ticket=${ticket}`}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              {movies.title}
-            </a>
-            <h4>{movies.torrents.en['1080p'].provider}</h4>
-            <img src={movies.images.banner} alt={movies.title} />
           </div>
         ))}
 
